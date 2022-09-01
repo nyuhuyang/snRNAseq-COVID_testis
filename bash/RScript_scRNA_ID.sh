66eebc03dfd6d20e26e3870b4ca8c823ea9a2281
Yang Hu <hu.yang@nyu.edu>
2022-09-01 12:05:38 -0400

    SingleR_bioconductor.R

--- bash/RScript_scRNA_ID.sh
+++ bash/RScript_scRNA_ID.sh
@@ -13,6 +13,6 @@ echo "Job ID : $JOB_ID"  ${SLURM_ARRAY_TASK_ID}
 conda activate r4.1.1
 path=/athena/elementolab/scratch/yah2014/Projects/snRNAseq-COVID_testis
 cd $path
-file=R/Rscripts/differential_expression.R
+file=R/Rscripts/SingleR_bioconductor.R
 echo $(ls -l $path/$file)
 Rscript $path/$file ${SLURM_ARRAY_TASK_ID}
